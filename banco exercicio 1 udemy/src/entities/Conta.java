4373f7bb00fdf0c2a4e43c8230436a8da987a536
luis.felipe <luis.felipe@TI-0491.cosmetologia.local>
2020-09-29 15:18:43 -0300

    classe desenvolvida

--- banco exercicio 1 udemy/src/entities/Conta.java
+++ banco exercicio 1 udemy/src/entities/Conta.java
@@ -1,6 +1,43 @@
 package entities;
 
 public class Conta {
+	private int numero;
+	private String nome;
+	private double valor;
+	
+	public Conta(int numero, String nome, double valor) {
+		super();
+		this.numero = numero;
+		this.nome = nome;
+		this.valor = valor;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public double getValor() {
+		return valor;
+	}
+
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
+	
+	
+	
 	
 
 }
